9e0ac6f6d3b6ce8d8b5cc8dce22f59157718a173
Yassar Hammami <yassarhammami@gmail.com>
2022-05-26 18:06:50 +0300

    Update RegistrationApplication.java

--- src/main/java/com/jazara/icu/registration/RegistrationApplication.java
+++ src/main/java/com/jazara/icu/registration/RegistrationApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableDiscoveryClient
 @EnableFeignClients
