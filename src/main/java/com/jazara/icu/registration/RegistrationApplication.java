57ad0ae9d0a65ede363d792bd8776b6f98878c78
Yassar Hammami <yassarhammami@gmail.com>
2022-07-22 05:34:54 +0300

    Update RegistrationApplication.java

--- src/main/java/com/jazara/icu/registration/RegistrationApplication.java
+++ src/main/java/com/jazara/icu/registration/RegistrationApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableEncryptableProperties
+@EnableSwagger2
 public class RegistrationApplication {
 
     public static void main(String[] args) {
