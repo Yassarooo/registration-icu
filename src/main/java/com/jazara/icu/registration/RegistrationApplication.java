125b382bca04a9dc583a0ed697f7d8d400a3fcfd
Yassar Hammami <yassarhammami@gmail.com>
2022-07-29 00:36:35 +0300

    .

--- src/main/java/com/jazara/icu/registration/RegistrationApplication.java
+++ src/main/java/com/jazara/icu/registration/RegistrationApplication.java
@@ -3,6 +3,7 @@ package com.jazara.icu.registration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableEncryptableProperties
+@EnableConfigurationProperties
 @EnableSwagger2
 @EntityScan(basePackages = {"com.jazara.icu.registration"})
 public class RegistrationApplication {
