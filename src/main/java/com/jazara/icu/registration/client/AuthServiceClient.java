d013270189bfaed6aa1cf8a257b3384921e00a51
Yassar Hammami <yassarhammami@gmail.com>
2022-07-30 00:52:56 +0300

    Update AuthServiceClient.java

--- src/main/java/com/jazara/icu/registration/client/AuthServiceClient.java
+++ src/main/java/com/jazara/icu/registration/client/AuthServiceClient.java
@@ -7,19 +7,23 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @FeignClient(name = "auth-service", fallback = AuthServiceClient.AuthServiceClientClientFallback.class)
 public interface AuthServiceClient {
 
     @PostMapping(value = "/api/auth/register")
+    @CrossOrigin(origins = "*", maxAge = 3600)
     ResponseEntity<Map<String, Object>> registerUserAccount(UserDTO user);
 
     @PostMapping(value = "/api/auth/activate")
+    @CrossOrigin(origins = "*", maxAge = 3600)
     ResponseEntity<Map<String, Object>> activateUserAccount(String emailorusername);
 
     @Component
